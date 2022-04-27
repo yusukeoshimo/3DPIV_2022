41b85d55a6b4015cb460fe780357c7f73b0ef20d
yusukeoshimo <70366730+yusukeoshimo@users.noreply.github.com>
2022-04-27 13:10:30 +0900

    mk_labelに拡張性を持たせた

--- experiment/LightGBM/func/mk_label.py
+++ experiment/LightGBM/func/mk_label.py
@@ -9,21 +9,17 @@ from PIL import Image
 import glob
 from tqdm import tqdm
 
-def video2label(original_video_path, label, memmap_path):
-    video_path = original_video_path
-    cap = cv2.VideoCapture(video_path) #読み込む動画のパス
-    frame_num = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+def mk_label(frame_num, label, memmap_path):
     size = (frame_num)
-    cap.release()
     
     memmap = np.memmap(memmap_path, dtype='uint8', mode='w+', shape=size)
     memmap[:] = memmap + label # memmapは［：］がないとファイルの中身が書き変わらない
 
 
 if __name__ == '__main__':
-    original_video_path = input('original video path >')
     label = int(input('input label >'))
-    file_name = os.path.splitext(os.path.basename(original_video_path))[0]
-    memmap_path = os.path.join(input('input dir to save memmap >'), '{}_output_{}.npy'.format(file_name, label))
+    frame_num = int(input('input label num >'))
+    file_name = input('input file name >')
+    memmap_path = os.path.join(input('input dir to save memmap >'), file_name)
     
-    video2label(original_video_path, label, memmap_path)
+    mk_label(frame_num, label, memmap_path)
