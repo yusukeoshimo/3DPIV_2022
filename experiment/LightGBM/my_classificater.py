6330e1919adac988172f73d36ff440299c801a3b
yusukeoshimo <70366730+yusukeoshimo@users.noreply.github.com>
2022-05-09 12:29:06 +0900

    my_classificater.pyのリファクタリング

--- experiment/LightGBM/my_classificater.py
+++ experiment/LightGBM/my_classificater.py
@@ -12,6 +12,8 @@ if __name__ == '__main__':
     dir_2 = r'C:\Users\yusuk\Desktop\3DPIV_2022\data\memmap_for_LightGBM\2'
     dir_1 = r'C:\Users\yusuk\Desktop\3DPIV_2022\data\memmap_for_LightGBM\1'
     dir_0 = r'C:\Users\yusuk\Desktop\3DPIV_2022\data\memmap_for_LightGBM\0'
+    width = 960
+    height = 1280
     video_mem_path = input('input target memmap >')
     
     
@@ -21,12 +23,12 @@ if __name__ == '__main__':
     x = np.memmap(memmap_path, dtype='float32', mode='r').reshape(-1, data_len)
     y = clf.predict(x)
     
-    video_mem = np.memmap(video_mem_path, dtype='uint8', mode='r').reshape(-1, 960, 1280)
+    video_mem = np.memmap(video_mem_path, dtype='uint8', mode='r').reshape(-1, width, height)
     
     for i, img in enumerate(tqdm(video_mem)):
         if y[i] == 0:
-            cv2.imwrite(os.path.join(dir_0,'_{}.bmp'.format(i)), img)
+            cv2.imwrite(os.path.join(dir_0,'{}.bmp'.format(i)), img)
         elif y[i] == 1:
-            cv2.imwrite(os.path.join(dir_1,'_{}.bmp'.format(i)), img)
+            cv2.imwrite(os.path.join(dir_1,'{}.bmp'.format(i)), img)
         elif y[i] == 2:
-            cv2.imwrite(os.path.join(dir_2,'_{}.bmp'.format(i)), img)
+            cv2.imwrite(os.path.join(dir_2,'{}.bmp'.format(i)), img)
