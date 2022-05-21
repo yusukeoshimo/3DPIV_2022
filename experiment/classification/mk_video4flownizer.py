218b12015e912b835b2cb72e14aaaccaf142b778
yusukeoshimo <70366730+yusukeoshimo@users.noreply.github.com>
2022-05-21 14:12:28 +0900

    mk_video4flownizer.pyに説明文を追

--- experiment/classification/mk_video4flownizer.py
+++ experiment/classification/mk_video4flownizer.py
@@ -6,22 +6,22 @@ from tqdm import tqdm
 
 
 if __name__ == '__main__':
-    dir_path = input('input dir path saved memmap >')
+    dir_path = input('input dir path saved memmap >') # memmapが保存されているディレクトリ．
     os.chdir(dir_path)
-    save_dir = input('input save dir >')
-    step = 2
-    cut_edge = 2 # 動画の両端をいくら切り捨てるか
-    pulse_order = 'second'
+    save_dir = input('input save dir >') # 作成した動画を保存するディレクトリ
+    step = 2 # 取得フレームがどれだけ離れたフレームか．1~
+    cut_edge = 2 # 動画の両端をいくら切り捨てるか．0~
+    pulse_order = 'second' # レーザーが点灯する順番．first or seconde
     if pulse_order == 'second':
-        extract_0 = 0+cut_edge
-        extract_1 = extract_0+step
+        extract_0 = 0+cut_edge # 取得するフレーム．
+        extract_1 = extract_0+step # 取得するフレーム
     else:
-        extract_1 = -1-cut_edge
-        extract_0 = -extract_1-step
+        extract_1 = -1-cut_edge # 取得するフレーム
+        extract_0 = -extract_1-step # 取得するフレーム
     save_path = os.path.join(save_dir, 'step_{}.avi'.format(step))
-    height = 960
-    width = 1280
-    fps = 1
+    height = 960 # フレームの高さ
+    width = 1280 # フレームの幅
+    fps = 1 # 作成する動画のfps
     
     
     file_dict = {int(file_name.split('_')[0]) : file_name for file_name in os.listdir(dir_path)} # {xxx:'xxx_yyy.npy', ....}
