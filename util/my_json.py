b20728e953f75b31326df81dad3808b63b56be15
yusukeoshimo <70366730+yusukeoshimo@users.noreply.github.com>
2022-05-23 21:49:50 +0900

    add : apend_json.py

---
+++ util/my_json.py
@@ -0,0 +1,22 @@
+import os
+import json
+
+def apend_json(json_path, key, value):
+    if os.path.exists(json_path):
+        with open(json_path, 'r') as f:
+            d = json.load(f)
+    else:
+        d = {}
+    
+    d[key] = value
+    
+    with open(json_path, 'w') as f:
+        json.dump(d, f, indent=4)
+        
+if __name__ == '__main__':
+    cwd = input('input project dir >')
+    os.chdir(cwd)
+    dict_path = os.path.join('system', 'control_dict.json')
+    d = {'test3':None}
+    apend_json(dict_path, 'test3', d)
+    
