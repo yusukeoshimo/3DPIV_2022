2848e68235b13b3e596d086273d4eeef8b134a9d
3dpiv2022 <3dpiv2022@gmail.com>
2022-07-15 13:01:58 +0900

    fix: intensity_vs_time.py (マジックナンバーを削除)

--- others/intensity_vs_time.py
+++ others/intensity_vs_time.py
@@ -6,12 +6,18 @@ from matplotlib import pyplot as plt
 if __name__ == '__main__':
     # dir_path = input('input dir path saved memmap >')
     dir_path = input('input dir path saved data_2_memmap >')
+    width = 640
+    height = 240
+    frame = 60
+    fps = 120
+    turn_on_time = 0.5
+    
     files = os.listdir(dir_path)
     paths = [os.path.join(dir_path, file_name) for file_name in files]
     
-    df = pd.DataFrame(index=range(1, 110+1))
+    df = pd.DataFrame(index=range(1, frame+1))
     for file_path in paths:
-        arr = np.memmap(file_path, dtype='uint8', mode='r').reshape(-1, 960, 1280)
+        arr = np.memmap(file_path, dtype='uint8', mode='r').reshape(-1, height, width)
         intensity_avg = np.mean(arr, axis=(1, 2))
         df[os.path.basename(file_path)] = intensity_avg
     df['mean'] = df.mean(axis=1)
@@ -22,14 +28,14 @@ if __name__ == '__main__':
     df['2_upper'] = df['mean']+df['2_sigma']
     df['2_lower'] = df['mean']-df['2_sigma']
     
-    time = df.index.values/220
+    time = df.index.values/fps
     
     plt.title('intensity vs time')
     plt.xlabel('time')
     plt.ylabel('intensity')
     plt.plot(time, df['mean'], color='black', label='mean',
              marker='o', markersize=2, markerfacecolor='blue', markeredgecolor="blue")
-    plt.xlim((0, 0.5))
+    plt.xlim((0, turn_on_time))
     plt.fill_between(time, df['1_lower'], df['1_upper'], alpha=0.7, label="$1\sigma$", color='red')
     plt.fill_between(time, df['2_lower'], df['2_upper'], alpha=0.3, label="$2\sigma$", color='red')
     plt.legend(loc=1)
