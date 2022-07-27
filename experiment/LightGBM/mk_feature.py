862f06a21292f03dd50f98cd818acfd77b1bd005
3dpiv2022 <3dpiv2022@gmail.com>
2022-07-27 23:54:01 +0900

    fix: mk_feature.py (関数の引数の順番を変更)

--- experiment/LightGBM/mk_feature.py
+++ experiment/LightGBM/mk_feature.py
@@ -9,7 +9,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..\..'))
 from util.my_json import read_json, apend_json, write_json
 
 class MkFeature():
-    def main(self, video_memmap_path, feature_memmap_path, width, height):
+    def main(self, feature_memmap_path, video_memmap_path, width, height):
         # extract features from improvement video
         arr = np.memmap(video_memmap_path, dtype='uint8', mode='r').reshape(-1, height, width)
         for i, img in enumerate(tqdm(arr)):
@@ -26,10 +26,10 @@ class MkFeature():
 if __name__ == '__main__':
     # # video2memmap to improve LightGBM
     video_path = input('input video path >')
+    video_memmap_path = input('input videmo memmap path >')
     cap = cv2.VideoCapture(video_path) #読み込む動画のパス
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     save_dir = input('input save dir >')
     mk_target = MkFeature()
-    mk_target.main(os.path.join(save_dir, 'video.npy'), os.path.join(save_dir, 'features.npy'), width=width, height=height)
-    print(mk_target.feature_num)
+    mk_target.main(os.path.join(save_dir, 'features.npy'), video_memmap_path, width=width, height=height)
