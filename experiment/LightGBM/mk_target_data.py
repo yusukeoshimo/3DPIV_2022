434b60746b2f632c2e7654e7c230ca912fb9f818
3dpiv2022 <3dpiv2022@gmail.com>
2022-07-27 15:28:12 +0900

    fix: mk_target_data.py (動画memmap生成の機能を分離)

--- experiment/LightGBM/mk_target_data.py
+++ experiment/LightGBM/mk_target_data.py
@@ -8,54 +8,28 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..\..'))
 from util.my_json import read_json, apend_json, write_json
 
+class MkFeature():
+    def main(self, video_memmap_path, feature_memmap_path, width, height):
+        # extract features from improvement video
+        arr = np.memmap(video_memmap_path, dtype='uint8', mode='r').reshape(-1, height, width)
+        for i, img in enumerate(tqdm(arr)):
+            ext = ExtractFeatures(img)
+            ext.extract_over_threshold(200)
+            ext.extract_over_threshold(150)
+            ext.extract_over_threshold(100)
+            if i == 0:
+                new_arr = np.memmap(feature_memmap_path, dtype='float32', mode='w+', shape=(arr.shape[0], ext.features.shape[0]))
+            new_arr[i] = ext.features
+    
+        self.feature_num = ext.features.shape[0]
 
 if __name__ == '__main__':
-    project_dir_path = input('input project dir path >')
-    position_dir_name = input('input position dir (side or bottom) >')
-    json_path = os.path.join(project_dir_path, 'system', 'control_dict.json')
-    control_dict = read_json(json_path)
-    cwd = control_dict[position_dir_name]['LightGBM_dir_path']
-    os.chdir(cwd)
-    
-    # video2memmap to improve LightGBM
-    target_video = control_dict[position_dir_name]['raw_video_path']
-    target_memmap = 'target.npy'
-    video2memmap(target_video, target_memmap)
-    
-    # target_memmap_pathをjsonファイルに記入
-    control_dict = read_json(json_path)
-    control_dict[position_dir_name]['target_memmap_path'] = os.path.join(cwd, target_memmap)
-    write_json(json_path, control_dict)
-    
-    cap = cv2.VideoCapture(target_video) #読み込む動画のパス
-    frame_num = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    # # video2memmap to improve LightGBM
+    video_path = input('input video path >')
+    cap = cv2.VideoCapture(video_path) #読み込む動画のパス
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    fps = int(cap.get(cv2.CAP_PROP_FPS))
-    
-    control_dict = read_json(json_path)
-    control_dict[position_dir_name]['video_width'] = width
-    control_dict[position_dir_name]['video_height'] = height
-    control_dict[position_dir_name]['video_frame_num'] = frame_num
-    control_dict[position_dir_name]['video_fps'] = fps
-    write_json(json_path, control_dict)
-    
-    # extract features from improvement video
-    arr = np.memmap(target_memmap, dtype='uint8', mode='r').reshape(-1, height, width)
-    for i, img in enumerate(tqdm(arr)):
-        ext = ExtractFeatures(img)
-        # ext.extract_std(0.1)
-        # ext.extract_mean()
-        # ext.extract_all_values((3, 1))
-        ext.extract_over_threshold(200)
-        ext.extract_over_threshold(150)
-        ext.extract_over_threshold(100)
-        if i == 0:
-            target_feature = 'target_features_{}.npy'.format(ext.features.shape[0])
-            new_arr = np.memmap(target_feature, dtype='float32', mode='w+', shape=(arr.shape[0], ext.features.shape[0]))
-        new_arr[i] = ext.features
-    
-    control_dict = read_json(json_path)
-    control_dict[position_dir_name]['target_feature_path'] = os.path.join(cwd, target_feature)
-    control_dict[position_dir_name]['features_num'] = ext.features.shape[0]
-    write_json(json_path, control_dict)
+    save_dir = input('input save dir >')
+    mk_target = MkFeature()
+    mk_target.main(os.path.join(save_dir, 'video.npy'), os.path.join(save_dir, 'features.npy'), width=width, height=height)
+    print(mk_target.feature_num)
