6b35222309059bb7ae57c4b5c9d6f8319b389e56
3dpiv2022 <3dpiv2022@gmail.com>
2022-07-27 15:27:05 +0900

    clean: video2memmap.py (リファクタリング)

--- experiment/LightGBM/func/video2memmap.py
+++ experiment/LightGBM/func/video2memmap.py
@@ -9,8 +9,8 @@ from PIL import Image
 import glob
 from tqdm import tqdm
 
-def video2memmap(original_video_path, memmap_path):
-    video_path = original_video_path
+
+def video2memmap(video_path, memmap_path):
     cap = cv2.VideoCapture(video_path) #読み込む動画のパス
     frame_num = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -18,19 +18,14 @@ def video2memmap(original_video_path, memmap_path):
     size = (frame_num, height, width)
     
     memmap = np.memmap(memmap_path, dtype='uint8', mode='w+', shape=size)
-    print(memmap.shape)
-
     for i in tqdm(range(frame_num)):
         ret, frame = cap.read()
         frame = frame[:,:,0] #(height, width, 3) -> (height, width)
-
         memmap[i] = frame # 書き込み
-        
     cap.release()
 
 if __name__ == '__main__':
-    original_video_path = input('original video path >')
-    file_name = os.path.splitext(os.path.basename(original_video_path))[0]
-    memmap_path = os.path.join(input('input dir to save memmap >'), '{}_input.npy'.format(file_name))
-
-    video2memmap(original_video_path, memmap_path)
+    video_path = input('video path >')
+    file_name = os.path.splitext(os.path.basename(video_path))[0]
+    memmap_path = os.path.join(input('input dir to save memmap >'), '{}.npy'.format(file_name))
+    video2memmap(video_path, memmap_path)
