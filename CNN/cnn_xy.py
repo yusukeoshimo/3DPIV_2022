b6e87500b5a65ef0e9159d07e119697164d0737a
yusukeoshimo <70366730+yusukeoshimo@users.noreply.github.com>
2022-09-01 17:03:02 +0900

    fix: cnn_xy.py (optunaでハイパーパラメータ最適化できるように変更)

--- CNN/cnn_xy.py
+++ CNN/cnn_xy.py
@@ -1,9 +1,11 @@
-from re import X
 import tensorflow.keras as keras
 from tensorflow.keras.layers import Conv2D, BatchNormalization, ReLU, Flatten, Dense, MaxPool2D
 import tensorflow as tf
 import numpy as np
 import math
+from tensorflow.keras.optimizers import Adam
+from keras.callbacks import EarlyStopping
+import optuna
 
 class FitGen(keras.utils.Sequence):
     '''
@@ -26,47 +28,73 @@ class FitGen(keras.utils.Sequence):
     def __len__(self):
         return self.length
 
-# データの形
-H, W, C = 32, 32, 2
-label_num = 3
+def create_model(mid_units):
+    H, W, C = 32, 32, 2
+    label_num = 3
+    # モデルの定義
+    inputs = tf.keras.layers.Input(shape=(H,W,C), name="inputs")
+    conv1 = Conv2D(filters=200, kernel_size=(8, 8), strides=4)(inputs)
+    conv1 = BatchNormalization()(conv1)
+    conv1 = ReLU()(conv1)
+    conv3_flat = Flatten()(conv1)
+    fc1 = Dense(200,activation='relu')(conv3_flat)
+    fc2 = Dense(mid_units[0], activation='relu')(fc1)
+    outputs = Dense(2, activation='linear')(fc2)
+    
+    model = keras.Model(inputs, outputs)
+    
+    # 学習方法の設定
+    model.compile(optimizer='adam',loss='mae')
+    
+    model.summary()
+    
+    return model
 
-# データの読み込み
-train_x_path = r'c:\Users\yusuk\Documents\3dpiv_2022\cnn\data\input.npy'
-x_train = np.memmap(train_x_path, mode='r', dtype=np.float16).reshape(-1, H, W, C)
-train_y_path = r'c:\Users\yusuk\Documents\3dpiv_2022\cnn\data\label.npy'
-y_train = np.memmap(train_y_path, mode='r', dtype=np.float16).reshape(-1, label_num)[:,:2]
+def objective(trial):
+    # データの形
+    H, W, C = 32, 32, 2
+    label_num = 3
 
-# モデルの定義
-inputs = tf.keras.layers.Input(shape=(H,W,C), name="inputs")
-conv1 = Conv2D(filters=32, kernel_size=(3, 3))(inputs)
-conv1 = BatchNormalization()(conv1)
-conv1 = ReLU()(conv1)
-conv1 = MaxPool2D((2, 2))(conv1)
-conv2 = Conv2D(64, (3, 3))(conv1)
-conv2 = BatchNormalization()(conv2)
-conv2 = ReLU()(conv2)
-conv2 = MaxPool2D((2, 2))(conv2)
-conv3 = Conv2D(64, (3, 3))(conv2)
-conv3 = BatchNormalization()(conv3)
-conv3 = ReLU()(conv3)
-conv3_flat = Flatten()(conv3)
-fc1 = Dense(64,activation='relu')(conv3_flat)
-outputs = Dense(2,activation='linear')(fc1)
+    # トレーニングデータの読み込み
+    train_x_path = r'c:\Users\yusuk\Documents\3dpiv_2022\cnn\data\input.npy'
+    x_train = np.memmap(train_x_path, mode='r', dtype=np.float16).reshape(-1, H, W, C)
+    train_y_path = r'c:\Users\yusuk\Documents\3dpiv_2022\cnn\data\label.npy'
+    y_train = np.memmap(train_y_path, mode='r', dtype=np.float16).reshape(-1, label_num)[:,:2]
+    # 検証用データの読み込み
+    test_x_path = r'c:\Users\yusuk\Documents\3dpiv_2022\cnn\data\test_input.npy'
+    x_validation = np.memmap(test_x_path, mode='r', dtype=np.float16).reshape(-1, H, W, C)
+    test_y_path = r'c:\Users\yusuk\Documents\3dpiv_2022\cnn\data\test_label.npy'
+    y_validation = np.memmap(test_y_path, mode='r', dtype=np.float16).reshape(-1, label_num)[:,:2]
 
-model = keras.Model(inputs, outputs)
+    # ハイーパーパラメータの定義
+    num_layer = 1
+    mid_unit_1 = trial.suggest_int(name='mid_units_1', low=50, high=300, step=1)
+    
+    # モデルの定義
+    inputs = tf.keras.layers.Input(shape=(H,W,C), name="inputs")
+    conv1 = Conv2D(filters=200, kernel_size=(8, 8), strides=4)(inputs)
+    conv1 = BatchNormalization()(conv1)
+    conv1 = ReLU()(conv1)
+    conv3_flat = Flatten()(conv1)
+    fc1 = Dense(200,activation='relu')(conv3_flat)
+    fc2 = Dense(mid_unit_1, activation='relu')(fc1)
+    outputs = Dense(2, activation='linear')(fc2)
+    # 層を組み立てる
+    model = keras.Model(inputs, outputs)
+    # 学習方法の設定
+    model.compile(optimizer='adam',loss='mae')
+    # モデルの表示
+    model.summary()
+    
+    # 学習
+    patience = 5
+    early_stopping = EarlyStopping(patience=patience, restore_best_weights=True)
+    model.fit_generator(generator=FitGen(x_train, y_train, batch_size=50),validation_data=(x_validation, y_validation), epochs=50, callbacks=[early_stopping])
+    
+    # 検証用データの損失地を計算
+    test_loss = model.evaluate(x_validation, y_validation, verbose=0)
+    return test_loss
 
-# 学習方法の設定
-model.compile(optimizer='adam',loss='mae')
-
-model.summary()
-model.fit_generator(generator=FitGen(x_train, y_train, batch_size=50), epochs=50)
-
-# 評価データに対する評価
-test_x_path = r'c:\Users\yusuk\Documents\3dpiv_2022\cnn\data\test_input.npy'
-x_test = np.memmap(test_x_path, mode='r', dtype=np.float16).reshape(-1, H, W, C)
-x_test = np.array(x_test)
-test_y_path = r'c:\Users\yusuk\Documents\3dpiv_2022\cnn\data\test_label.npy'
-y_test = np.memmap(test_y_path, mode='r', dtype=np.float16).reshape(-1, label_num)
-y_test = np.array(y_test)[:,:2]
-test_loss = model.evaluate(x_test, y_test, verbose=0)
-print('test data loss:', test_loss)
+if __name__ == '__main__':
+    study = optuna.create_study()
+    study.optimize(objective, n_trials=3)
