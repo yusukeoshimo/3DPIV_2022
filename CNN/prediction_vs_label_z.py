092a40bfd9485f725f50f753dc314c166afc3f23
yusukeoshimo <70366730+yusukeoshimo@users.noreply.github.com>
2022-12-20 16:30:59 +0900

    fix: prediction_vs_label_z.py (データ数の指定，軸目盛りの指定を可能に)

--- CNN/prediction_vs_label_z.py
+++ CNN/prediction_vs_label_z.py
@@ -8,11 +8,11 @@ model_path = input('input model path >')
 X_path = input('input input-data >')
 label_path = input('input label path >')
 save_dir = input('input save dir >')
-
+data_num = -1
 model = load_model(model_path)
-X0 = np.memmap(X_path, np.uint8, 'r',).reshape(-1, 32, 32, 4)[:,:,:,:2]
-X1 = np.memmap(X_path, np.uint8, 'r',).reshape(-1, 32, 32, 4)[:]
-label = np.memmap(label_path, np.float16, 'r').reshape(-1, 3)[:,2]
+X0 = np.memmap(X_path, np.uint8, 'r',).reshape(-1, 32, 32, 4)[:data_num,:,:,:2]
+X1 = np.memmap(X_path, np.uint8, 'r',).reshape(-1, 32, 32, 4)[:data_num]
+label = np.memmap(label_path, np.float16, 'r').reshape(-1, 3)[:data_num,2]
 
 Y = model.predict([X0, X1])
 
@@ -29,6 +29,8 @@ plt.xlabel('label')
 plt.ylabel('prediction')
 plt.xlim((-1,9))
 plt.ylim((-1,9))
+plt.xticks(range(-1,10,1))
+plt.yticks(range(-1,10,1))
 plt.scatter(label, Y, s=0.5, alpha=0.1)
 ax.set_aspect('equal')
 plt.plot([0,8],[a*0+b, a*8+b], color='red')
